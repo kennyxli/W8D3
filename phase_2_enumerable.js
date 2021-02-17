9940ae4ee066723949edf2f40b663fbb1afb82e3
Kenny Li <kenny.wj.li@gmail.com>
2021-02-17 17:09:17 -0500

    finished myreduce and mymap

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -1,12 +1,42 @@
-let callback = function(num){
-    return num + 1;
-}
-Array.prototype.myEach = function(callback)
-{   
-    let arr = [];
-    for(let i = 0; i < this.length; i++)
-    {
-        arr.push(callback(this[i]));
+let callback1 = function(ele) {
+    console.log(ele + 1);
+};
+
+Array.prototype.myEach = function(callback) {   
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i]);
     }
+};
+
+Array.prototype.myMap = function (callback) {
+    let arr = [];
+    
+    this.myEach(function(ele) {
+        arr.push(callback(ele))
+    }) 
+    return arr
     console.log(arr);
-}
+};
+
+
+
+Array.prototype.myReduce = function(callback, initialvalue = 0){
+    
+    this.myEach(function(ele){
+        initialvalue = callback(initialvalue, ele)
+    });
+    return initialvalue
+    console.log(initialvalue)
+};
+
+
+
+function add(num){console.log(num)}
+[1, 2, 3].myEach(function add(num) { console.log(num) })
+
+def my_map(&prc)
+    arr =[]
+    self.each do |ele|
+        arr << prc.call(ele)
+    end
+end
